7ccadca430b173a9b870d9bae3ddb5cf74d64e9c
Katerina Ignatova <katerina.d.ignatova@gmail.com>
2018-08-21 16:09:10 +0300

    TASK-1 added webpack-dev-server. scss loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,14 @@
+const autoprefixer = require('autoprefixer');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+
 module.exports = (env, argv) => {
   const isDev = argv.mode === 'development';
   return {
+    resolve: {
+      extensions: ['.js', '.jsx', '.scss']
+    },
     module: {
       rules: [
         {
@@ -9,8 +17,44 @@ module.exports = (env, argv) => {
           use: {
             loader: "babel-loader"
           }
+        },
+        {
+          test: /\.scss$/,
+          use: [
+              MiniCssExtractPlugin.loader,
+              {
+                  loader: 'css-loader',
+                  options: {
+                      importLoaders: 2,
+                      sourceMap: true
+                  }
+              },
+              {
+                  loader: 'postcss-loader',
+                  options: {
+                      ident: 'postcss',
+                      plugins: () => [
+                          autoprefixer({
+                              browsers: ['> 0.1%']
+                          })
+                      ]
+                  }
+              },
+              {
+                  loader: 'sass-loader'
+              }
+          ]
         }
       ]
-    }
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html'
+      }),
+      new MiniCssExtractPlugin({
+        filename: "[name].css",
+        chunkFilename: "[id].css"
+      })
+    ]
   }
 }
