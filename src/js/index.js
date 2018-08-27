45842be6ed7ab6c90143a0b247ea6ec8c603e579
Katerina Ignatova <katerina.d.ignatova@gmail.com>
2018-08-27 11:32:26 +0300

    TASK-2 remove store js

--- src/js/index.js
+++ src/js/index.js
@@ -2,10 +2,25 @@ import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+import {
+  applyMiddleware,
+  compose,
+  createStore
+} from 'redux';
+import thunk from 'redux-thunk';
 
 import '../assets/styles/main.scss';
-import store from './store/index';
+import rootReducer from './reducers/index';
 import App from './components/App';
+
+let composeEnhancers = compose;
+let middleware = [thunk];
+
+if (process.env.NODE_ENV === 'develop') {
+    composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || composeEnhancers;
+}
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(...middleware)));
 
 render(
   <Provider store={store}>
